b6113d48a5498ff74113b4613e1221848f7447a2
hema <hema.gowda1991@gmail.com>
2020-07-19 19:18:18 +0530

    My First Commit

---
+++ Sample/src/com/edureka/gitdemo/MyFirstGitCheckin.java
@@ -0,0 +1,10 @@
+package com.edureka.gitdemo;
+
+public class MyFirstGitCheckin {
+
+	public static void main(String[] args) {
+		System.out.println("Git is easy to configure!!!");
+		System.out.println("Changed");
+	}
+
+}
